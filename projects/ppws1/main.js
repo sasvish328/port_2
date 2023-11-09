dfb5935be7260941aa53cf1ff4411c622de845cb
sasvish328 <bkm2@ukr.net>
2023-11-09 16:48:32 +0200

    after diring in ppws1 and corr afte Sergey analysis

--- projects/ppws1/main.js
+++
@@ -1,5 +0,0 @@
-const el = document.querySelector(".begin-form-box");
-let computedStyle = window.getComputedStyle(el);
-// let fontSize = computedStyle.getPropertyValue("font-size");
-let width = computedStyle.getPropertyValue("width");
-console.log("Ширина блока:", width);
